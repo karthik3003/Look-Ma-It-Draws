5668f89c07c5a460e703a1acc8a164b815ab1d14
Laddipeerla,Karthik <s528805@ch140032m.nwmissouri.edu>
2017-11-27 21:32:48 -0600

    created squiggle

--- Look, Ma, It Draws/ViewController.swift
+++ Look, Ma, It Draws/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBAction func buttonRed(_ sender: Any) {
+        UIColor.red.setStroke()
+    }
+    
+    @IBAction func buttonOrange(_ sender: Any) {
+        UIColor.orange
+    }
+    
+    @IBAction func buttonYellow(_ sender: Any) {
+        UIColor.yellow
+    }
+    
+    @IBAction func buttonGreen(_ sender: Any) {
+        UIColor.green
+    }
+    
+    @IBAction func buttonBlue(_ sender: Any) {
+        UIColor.blue
+    }
+    
+    @IBAction func buttonPurple(_ sender: Any) {
+        UIColor.purple
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
